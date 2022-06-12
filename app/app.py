ce0d047f7b7cf8ef7a4ae022591fccf99cfaa205
malu <malu.vandenberg@student.fontys.nl>
2022-06-12 16:24:53 +0200

    working in db

--- app/app.py
+++ app/app.py
@@ -40,37 +40,19 @@ consumer = KafkaConsumer(
 )
 
 
-# value_deserializer=lambda x: loads(x.decode('utf-8')))
-
-
 @app.route('/hola')
 def hello_world():  # put application's code here
     collection.insert_one({"sentiment": "TEST MESSAGE"})
     return "TEST"
 
 
-# async def run():
-
-
 if __name__ == '__main__':
     # listen_kill_server()
     while True:
-        print("....listening kafka")
-        # this method should auto-commit offsets as you consume them.
-        # If it doesn't, turn on logging.DEBUG to see why it gets turned off.
-        # Not assigning a group_id can be one cause
-        # logging.DEBUG
-        print(consumer.topics())
         for message in consumer:
             print(".....ongoing transaction")
-            # consumed_message = json.loads(message.value.decode())
-            print(message)
-            collection.insert_one({"sentiment": "NEW MESSAGE"})
-            print(message.topic)
-            print(message.partition)
-            print(message.offset)
-            print(message.key)
-            print(consumer.topics())
+            collection.insert_one({"sentiment": message.value.decode()})
+            # json.loads(message.value.decode())
     app.run(host="0.0.0.0", port=8083, debug=True)
     # while istrue:
     #     run()
